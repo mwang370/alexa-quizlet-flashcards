ce5489abf4d5b2bcbb83eeaa7c6c7021bfc61f6d
mwang370 <mwang370@gatech.edu>
2016-12-28 02:31:29 -0500

    added more built in intents

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 module.change_code = 1;
+
+/*
+Declarations
+*/
 var _ = require('lodash');
 var Alexa = require('alexa-app');
 var skill = new Alexa.app('flashcards');
@@ -8,21 +12,17 @@ var WordBank = require('./word_bank.js');
 var currentSet = null;
 var currentWordBank = null;
 
-skill.launch(function(request, response) {
+/*
+Intent Helper Methods
+*/
+var launchIntentFunction = function(request, response) {
     var prompt = 'Ready to study? To begin, tell me the name of a set';
     var reprompt = 'I didn\'t hear a set name. Tell me a set name to begin studying';
     response.say(prompt).reprompt(reprompt).shouldEndSession(false);
-});
+}
 
-skill.intent('startStudyingIntent', {
-    'slots': {
-        'SETNAME': 'SETNAMES'
-    },
-    'utterances': [
-        '{start|open|begin} {|studying|reviewing} {|flashcards} {|for} {-|SETNAME}'
-    ]
-},
-function(request, response) {
+var startStudyingIntentFunction = function(request, response) {
+    console.log("Start studying event triggered");
     var setName = request.slot('SETNAME');
     var reprompt = 'Tell me a set name to begin studying';
     var accessToken = request.sessionDetails.accessToken;
@@ -31,7 +31,6 @@ function(request, response) {
         response.say(prompt).reprompt(reprompt).shouldEndSession(false);
         return true;
     } else {
-        console.log("set name: " + setName);
         var flashcardsHelper = new FlashcardsDataHelper();
         flashcardsHelper.getSets(accessToken).then(function(allSets) {
             for (var i = 0; i < allSets.length; i++) {
@@ -39,28 +38,100 @@ function(request, response) {
                     currentSet = allSets[i];
                 }
             }
-            var prompt = "I have just retrieved the set for " + setName + ". Your first card is undefined";
-            var reprompt = "Your first card is undefined";
+            if (currentSet !== null) {
+                currentWordBank = new WordBank();
+                for (var card in currentSet.terms) {
+                    var term = currentSet.terms[card].term;
+                    var definition = currentSet.terms[card].definition;
+                    currentWordBank.addWord(term, definition);
+                }
+                currentWordBank.shuffle();
+                console.log("word bank: ");
+                console.log(currentWordBank);
+                var currentWord = currentWordBank.getNextWord();
+                var prompt = "I have just retrieved the set for " + setName + ". Your first card is " + currentWord;
+                var reprompt = "Your first card is " + currentWord;
+            }
             if (currentSet === null) {
                 prompt = "I could not retrieve the set for " + setName + ". Tell me another set name."
                 reprompt = "Tell me another set name."
             }
-            console.log(currentSet);
             response.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
         }).catch(function(err) {
             console.log(err.statusCode);
-            var prompt = 'I could not retrieve the set.';
+            var prompt = 'I could not retrieve the set. Try repeating the set name or start another set.';
           response.say(prompt).reprompt(prompt).shouldEndSession(false).send();
         });
         return false;
     }
-});
-
-var cancelIntentFunction = function(request, response) {
-  response.say('Goodbye!').shouldEndSession(true);
 };
 
+var cancelIntentFunction = function(request, response) {
+    console.log("Cancel intent triggered");
+    response.say('Goodbye!').shouldEndSession(true);
+    return true;
+};
+
+var correctIntentFunction = function(request, response) {
+    console.log("Correct intent triggered");
+    currentWordBank.gotCorrect();
+    var prompt = "Good Job! Your next word is undefined.";
+    var reprompt = "Your next word is undefined.";
+    response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    console.log("word bank: ");
+    console.log(currentWordBank);
+    return true;
+};
+
+var wrongIntentFunction = function(request, response) {
+    console.log("Wrong intent function triggered");
+    currentWordBank.gotWrong();
+    var prompt = "Almost. We\'ll come back to that one later. Your next word is undefined.";
+    var reprompt = "Your next word is undefined.";
+    response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    console.log("word bank: ");
+    console.log(currentWordBank);
+    return true;
+};
+
+var skipIntentFunction = function(request, response) {
+    console.log("Skip intent function triggered");
+    currentWordBank.gotWrong();
+    var prompt = "OK, let\'s skip that one. We\'ll come back to that one later. Your next word is undefined.";
+    var reprompt = "Your next word is undefined.";
+    response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    console.log("word bank: ");
+    console.log(currentWordBank);
+    return true;
+};
+
+var repeatIntentFunction = function(request, response) {
+    console.log("Repeat intent function triggered");
+    var prompt = "No problem, I'll repeat it. Your word is undefined";
+    var reprompt = "Your word is undefined";
+    response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    console.log("word bank: ");
+    console.log(currentWordBank);
+    return true;
+}
+
+/*
+Intents
+*/
+skill.launch(launchIntentFunction);
+skill.intent('startStudyingIntent', {
+    'slots': {
+        'SETNAME': 'SETNAMES'
+    },
+    'utterances': [
+        '{start|open|begin|study} {|studying|reviewing} {|flashcards|cards|set} {|for} {-|SETNAME}'
+    ]
+}, startStudyingIntentFunction);
 skill.intent('AMAZON.CancelIntent', {}, cancelIntentFunction);
 skill.intent('AMAZON.StopIntent', {}, cancelIntentFunction);
+skill.intent('AMAZON.YesIntent', {}, correctIntentFunction);
+skill.intent('AMAZON.NoIntent', {}, wrongIntentFunction);
+skill.intent('AMAZON.NextIntent', {}, skipIntentFunction);
+skill.intent('AMAZON.RepeatIntent', {}, repeatIntentFunction);
 
 module.exports = skill;
--- word_bank.js
+++ word_bank.js
@@ -3,13 +3,39 @@ var _ = require('lodash');
 
 function WordBank() {
     this.numWords = 0;
+    this.numFinished = 0;
     this.words = new Array();
+    this.finished = new Array();
 };
 
 WordBank.prototype.addWord = function(front, back) {
-    this.words[this.numWords] = new Array(front, back, 0);
+    var newWord = new Array(front, back);
+    this.words[this.numWords] = newWord;
     this.numWords++;
 };
 
+WordBank.prototype.shuffle = function() {
+    var j, x, i;
+    for (i = this.words.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = this.words[i - 1];
+        this.words[i - 1] = this.words[j];
+        this.words[j] = x;
+    }
+}
+
+WordBank.prototype.getNextWord = function() {
+    return this.words[0][0];
+}
+
+WordBank.prototype.gotCorrect = function() {
+    this.finished.push(this.words.shift());
+    this.numFinished++;
+    this.numWords--;
+}
+
+WordBank.prototype.gotWrong = function() {
+    this.words.push(this.words.shift());
+}
 
 module.exports = WordBank;
